8263ad331b8da0ca56e3cd6efeb4eda5a6137db0
Jackie Jr <jwysinger@gmail.com>
2023-12-27 17:24:39 -0600

    added logo img and nav ul

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './Navbar.css'
+
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+
 const Navbar = () => {
   return (
     <div className='navbar'>
-      
+      <div className="nav-logo">
+        <img src={logo} />
+        <p>SHOPPER</p>
+      </div>
+      <ul className='nav-menu'></ul>
     </div>
   )
 }
