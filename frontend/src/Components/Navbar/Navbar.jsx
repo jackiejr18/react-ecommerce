165faac635bc8d60cd115cadbca92cbd85928f6b
Jackie Jr <jwysinger@gmail.com>
2023-12-28 09:28:51 -0600

    Navbar list with button and cart icon

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -11,7 +11,16 @@ const Navbar = () => {
         <img src={logo} />
         <p>SHOPPER</p>
       </div>
-      <ul className='nav-menu'></ul>
+      <ul className='nav-menu'>
+        <li>Shop</li>
+        <li>Men</li>
+        <li>Women</li>
+        <li>Kids</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon}/>
+      </div>
     </div>
   )
 }
