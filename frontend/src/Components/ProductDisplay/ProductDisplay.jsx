32bd0ea7920a2d8b3fc0b20a729cec183f1d8d16
Jackie Jr <jwysinger@gmail.com>
2024-01-06 12:24:34 -0600

    ProductDisplay compleete

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -20,35 +20,35 @@ const ProductDisplay = (props) => {
       </div>
       <div className='productdisplay-right'>
         <h1>{product.name}</h1>
-        <div className='productdisplay-right-star'>
+        <div className='productdisplay-right-stars'>
           <img src={star_icon}/>
           <img src={star_icon}/>
           <img src={star_icon}/>
           <img src={star_icon}/>
           <img src={star_dull_icon}/>
           <p>122</p>
-          <div className='productdisplay-right-prices'>
-            <div className='productdisplay-right-price-old'>${product.old_price}</div>
-            <div className='productdisplay-right-price-new'>${product.new_price}</div>
-          </div>
-          <div className='productdisplay-right-description'>
-            A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short
-            sleeves, worn as an undershirt or outer garment.
-          </div>
-          <div className='productdisplay-right-size'>
-            <h1>Select Size</h1>
-            <div className='productdisplay-right-size'>
-              <div>S</div>
-              <div>M</div>
-              <div>L</div>
-              <div>XL</div>
-              <div>XXL</div>
-            </div>
-          </div>
-          <button>ADD TO CART</button>
-          <p className='productdisplay-right-category'><span>Category :</span>Women, T-Shirt, Crop Top</p>
-          <p className='productdisplay-right-category'><span>Tags :</span>Modern, Latest</p>
         </div>
+        <div className='productdisplay-right-prices'>
+          <div className='productdisplay-right-price-old'>${product.old_price}</div>
+          <div className='productdisplay-right-price-new'>${product.new_price}</div>
+        </div>
+        <div className='productdisplay-right-description'>
+          A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short
+          sleeves, worn as an undershirt or outer garment.
+        </div>
+        <div className='productdisplay-right-size'>
+          <h1>Select Size</h1>
+          <div className='productdisplay-right-sizes'>
+            <div>S</div>
+            <div>M</div>
+            <div>L</div>
+            <div>XL</div>
+            <div>XXL</div>
+          </div>
+        </div>
+        <button>ADD TO CART</button>
+        <p className='productdisplay-right-category'><span>Category :</span>Women, T-Shirt, Crop Top</p>
+        <p className='productdisplay-right-category'><span>Tags :</span>Modern, Latest</p>
       </div>
     </div>
   )
